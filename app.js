b6cd937a15e8a857a6791917da23d47700ef1e3d
fadhil9 <fadhilmuhamad272@gmail.com>
2024-05-06 22:07:26 +0700

    add search bar

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 
-const h1 = document.getElementById("hello");
+// const h1 = document.getElementById("hello");
 
-let isRed= false;
+// let isRed= false;
 
-h1.addEventListener('click', function(){
-    if(isRed){ 
+// h1.addEventListener('click', function(){
+//     if(isRed){ 
         
-        h1.style.color = 'green';
-        isRed = false;
-    }else{ 
-        h1.style.color = 'red';
-        isRed = true;
-    }
+//         h1.style.color = 'green';
+//         isRed = false;
+//     }else{ 
+//         h1.style.color = 'red';
+//         isRed = true;
+//     }
 
-});
+// });
