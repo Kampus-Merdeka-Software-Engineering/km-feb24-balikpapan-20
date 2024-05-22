c3d8a5376ac4e6f92a7575fd529e9d92319a018c
fadhil9 <fadhilmuhamad272@gmail.com>
2024-05-23 01:00:41 +0700

    update login.css,index.js, tambah modal login register di index.html

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,164 +1,178 @@
-import express from "express";
-import bodyParser from "body-parser";
-import pg from "pg";
-import bcrypt from "bcrypt";
-import session from "express-session";
-import passport from "passport";
-import { Strategy } from "passport-local";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth,createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged,signOut } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import {getFirestore, setDoc,getDoc, doc} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js"
 
-// JANGAN DIUBAH2 YA GESS UNTUK FILE INI, KALO DASHBOARD SAMA RESPONSIVE UDH SELESAI TINGGAL INSTAL NODE MODULE SAMA RAPIHIN STRUKTUR
-// FOLDER
+const firebaseConfig = {
+  apiKey: "AIzaSyDT_AvkKXdz7bpBU0nd9NmqWvF_Y3RKXog",
+  authDomain: "maven-roaster.firebaseapp.com",
+  projectId: "maven-roaster",
+  storageBucket: "maven-roaster.appspot.com",
+  messagingSenderId: "1072729075137",
+  appId: "1:1072729075137:web:d01a95efea6bfe32009340"
+};
 
-const app = express();
-const port = 3000;
-const saltRounds = 9;
+alert('connect');
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth=getAuth();
+const db=getFirestore();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
 
-app.use(
-  session({
-    secret: "TOPSECRETWORD",
-    resave: false,
-    saveUninitialized: true,
-    // expired waktu cookies session jadi 24 jam
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24,
-    },
-  })
-);
 
-app.use(passport.initialize());
-app.use(passport.session());
+const dasboard =document.getElementById('dashboard')
+const closeL = document.querySelectorAll('.close')[0];
+const closeR = document.querySelectorAll('.close')[1];
+const openSignIn  =document.getElementById('loginModal')
+const openSignup  =document.getElementById('registerModal')
+const submitLogin = document.getElementById('submitLogin');
+const submitRegister = document.getElementById('submitRegister');
 
-const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "maven",
-  password: "#Pofadhilsky99",
-  port: 5432,
+
+//close modal login register
+closeL.addEventListener('click', function() {
+  openSignIn.style.display = 'none';
 });
-db.connect();
 
-app.get("/", (req, res) => {
-  if (req.isAuthenticated()) {
-    console.log(req.user.email);
-    res.render("home.ejs", { login: true });
-  } else {
-    res.render("home.ejs", { login: false });
+
+closeR.addEventListener('click', function() {
+  openSignup.style.display = 'none';
+});
+
+window.addEventListener('click', function(event) {
+  if (event.target === openSignIn) {
+    openSignIn.style.display = 'none';
+  }else if(event.target === openSignup){
+    openSignup.style.display = 'none';
   }
 });
 
-app.get("/login", (req, res) => {
-  res.render("login.ejs");
-});
 
-app.get("/register", (req, res) => {
-  res.render("register.ejs");
-});
 
-app.get("/aboutus", (req, res) => {
-  res.render("aboutus.ejs");
-});
-
-app.get("/dashboard", (req, res) => {
-  if (req.isAuthenticated()) {
-    res.render("dashboard.ejs");
-  } else {
-    res.redirect("/login");
-  }
-});
-
-app.get("/logout", (req, res) => {
-  req.logout(function (err) {
-    if (err) {
-      return next(err);
-    }
-    res.redirect("/");
-  });
-});
-
-app.post(
-  "/login",
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/login",
-  })
-);
-
-app.post("/register", async (req, res) => {
-  const email = req.body.username;
-  const password = req.body.password;
-
-  try {
-    const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [
-      email,
-    ]);
-
-    if (checkResult.rows.length > 0) {
-      req.redirect("/login");
-    } else {
-      bcrypt.hash(password, saltRounds, async (err, hash) => {
-        if (err) {
-          console.error("Error hashing password:", err);
-        } else {
-          const result = await db.query(
-            "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *",
-            [email, hash]
-          );
-          const user = result.rows[0];
-          req.login(user, (err) => {
-            console.log("success");
-            res.redirect("/");
-          });
-        }
-      });
-    }
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-passport.use(
-  new Strategy(async function verify(username, password, cb) {
-    try {
-      const result = await db.query("SELECT * FROM users WHERE email = $1 ", [
-        username,
-      ]);
-      if (result.rows.length > 0) {
-        const user = result.rows[0];
-        const storedHashedPassword = user.password;
-        bcrypt.compare(password, storedHashedPassword, (err, valid) => {
-          if (err) {
-            //Error with password check
-            console.error("Error comparing passwords:", err);
-            return cb(err);
-          } else {
-            if (valid) {
-              //Passed password check
-              return cb(null, user);
-            } else {
-              //Did not pass password check
-              return cb(null, false);
-            }
+// open modal login jika belum login
+dasboard.addEventListener('click', function(){
+  onAuthStateChanged(auth, (user)=>{
+    const loggedInUserId = localStorage.getItem('loggedInUserId');
+    if(loggedInUserId){
+      console.log(user);
+      const docRef = doc(db,"users",loggedInUserId);
+      getDoc(docRef)
+      .then((docSnap)=>{
+        
+          const userData= docSnap.data();
+          if(userData.role == 'admin'){
+            window.location.href='dashboard.html'
+          }else{
+            localStorage.removeItem('loggedInUserId');
+            signOut(auth);
+            alert('anda bukan admin')
           }
-        });
-      } else {
-        return cb("User not found");
-      }
-    } catch (err) {
-      console.log(err);
+      })
+      .catch((error)=>{
+        console.log(error);
+      })
+    }else{
+       openSignIn.style.display = 'block'
+       openSignup.style.display = 'none'
     }
   })
-);
+  
+})
 
-passport.serializeUser((user, cb) => {
-  cb(null, user);
-});
-passport.deserializeUser((user, cb) => {
-  cb(null, user);
+
+
+
+
+//open modal register
+document.getElementById('openRegisterModal').addEventListener('click',function(){
+  console.log('open modal');
+  openSignIn.style.display = 'none'
+  openSignup.style.display= 'block'
+})
+
+
+//submit register create account
+submitRegister.addEventListener("click",function(event) {
+  
+  event.preventDefault();
+
+  const email = document.getElementById('register-email').value;
+  const password = document.getElementById('register-password').value;
+  const firstName = document.getElementById('firstname').value;
+  const lastName = document.getElementById('lastname').value;
+
+  const auth = getAuth();
+  const db = getFirestore();
+
+  createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential)=>{
+    const user = userCredential.user;
+    const userData ={
+      email :email,
+      firstName : firstName,
+      lastName : lastName,
+      role : "user"
+    };
+    alert("created account");
+
+    const docRef = doc(db, "users",user.uid);
+    setDoc(docRef,userData)
+    .then(()=>{
+
+    openSignup.style.display = 'none';
+    openSignIn.style.display = 'block'; 
+    })
+    .catch((error)=>{
+     alert(error);
+    })
+  })
+  .catch((error)=>{
+    if(error.code === 'auth/email-already-in-use'){
+      alert('email addres already exist!');
+    }else{
+      alert(error.code)
+    }
+  })
+  
+})
+
+//submit login
+submitLogin.addEventListener("click",function (event) {   
+  event.preventDefault();
+  
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const auth=getAuth();
+
+
+signInWithEmailAndPassword (auth, email, password)
+.then((userCredential) => {
+  const user = userCredential.user;
+
+  localStorage.setItem('loggedInUserId', user.uid);
+
+  alert("login..")
+  
+  const docRef = doc(db,"users",user.uid);
+  getDoc(docRef)
+  .then((docSnap)=>{
+      const role = docSnap.data().role
+      if(role == 'admin'){
+        window.location.href='dashboard.html'
+      }else{
+        alert('maaf anda bukan admin')
+      }
+
+  })
+  
+})
+.catch((error) => {
+   const errorCode = error.code;
+
+  if(errorCode==='auth/invalid-credential'){
+    alert('Incorrect Email or Password');
+  }else{
+    alert('Account does not Exist');
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port: ${port}`);
-});
+})
