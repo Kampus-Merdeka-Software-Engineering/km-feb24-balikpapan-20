6284c49e5e8bd4e117b4516bb9871d59fc83d3b4
Yogi Kurnia Putra <168409372+tuanmudayogi@users.noreply.github.com>
2024-05-30 11:53:48 +0700

    Update product.js

--- assets/js/product.js
+++ assets/js/product.js
@@ -7,7 +7,7 @@
         "columns": [
             {   "data": "id_product" 
                 "render": function (data, type, row) {
-                    return "ID-" + data;
+                    return "ID" + data;
                 }
             },
             {   "data": "product" },
