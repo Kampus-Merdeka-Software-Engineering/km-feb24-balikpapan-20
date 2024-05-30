6044bd7ecce49feecffbad037e25447ea5d9d847
Yogi Kurnia Putra <168409372+tuanmudayogi@users.noreply.github.com>
2024-05-30 11:37:07 +0700

    Update product.js

--- assets/js/product.js
+++ assets/js/product.js
@@ -5,12 +5,7 @@
             "dataSrc": ""
         },
         "columns": [
-            {   
-                "data": "id_product",
-                "render": function (data, type, row) {
-                    return "ID " + data; // Menambahkan "ID" di depan data id_product
-                }
-            },
+            {   "data": "id_product" },
             {   "data": "product" },
             {   "data": "category" },
             { 
