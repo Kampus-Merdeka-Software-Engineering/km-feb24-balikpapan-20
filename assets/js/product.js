67196c1b6acdcb948fb082f2978ca0d01d59b23a
Yogi Kurnia Putra <168409372+tuanmudayogi@users.noreply.github.com>
2024-05-30 11:36:16 +0700

    Update product.js

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,33 +1,27 @@
-    function showTab(tabId) {
-            var tabs = document.getElementsByClassName('tab-content');
-            for (var i = 0; i < tabs.length; i++) {
-                tabs[i].classList.remove('active');
-            }
-            document.getElementById(tabId).classList.add('active');
-        }
+   $(document).ready(function() {
+    $('#listproduct').DataTable({
+        "ajax": {
+            "url": "assets/data/product.json",
+            "dataSrc": ""
+        },
+        "columns": [
+            {   
+                "data": "id_product",
+                "render": function (data, type, row) {
+                    return "ID " + data; // Menambahkan "ID" di depan data id_product
+                }
+            },
+            {   "data": "product" },
+            {   "data": "category" },
+            { 
+                "data": "price",
+                "render": function (data, type, row) {
+                    return "$" + data;
+                }
+            },
+            { "data": "type" }
+        ]
+    });
+});
 
-        $(document).ready(function() {
-            $('#listproduct').DataTable({
-                "ajax": {
-                    "url": "assets/data/product.json", // Pastikan jalur menuju file JSON benar
-                    "dataSrc": ""
-                },
-                "columns": [
-                    {   "data": "id_product" 
-                        "render": function (data, type, row) {
-                            return "ID-" + data; // Menambahkan dollar sign ($) di depan harga
-                        }
-                    },
-                    {   "data": "product" },
-                    {   "data": "category" },
-                    { 
-                        "data": "price",
-                        "render": function (data, type, row) {
-                            return "$" + data; // Menambahkan dollar sign ($) di depan harga
-                        }
-                    },
-                    { "data": "type" }
-                ]
-            });
-        });
         
