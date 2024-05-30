707d9d823a3bab8238b021febd56abcaa0c93f3d
Yogi Kurnia Putra <168409372+tuanmudayogi@users.noreply.github.com>
2024-05-30 13:16:07 +0700

    Update product.js

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,4 +1,25 @@
 $(document).ready(function() {
+    // Fungsi untuk mengatur lebar kolom
+    function setColumnWidths() {
+        $('#listproduct thead th').each(function() {
+            var textWidth = $(this).textWidth();
+            $(this).width(textWidth + 20); // Tambahkan padding tambahan jika diperlukan
+        });
+    }
+
+    // Fungsi untuk mengukur lebar teks
+    $.fn.textWidth = function() {
+        var htmlOrg = $(this).html();
+        var htmlCalc = '<span>' + htmlOrg + '</span>';
+        $(this).html(htmlCalc);
+        var width = $(this).find('span:first').width();
+        $(this).html(htmlOrg);
+        return width;
+    };
+
+    // Panggil fungsi setColumnWidths saat tabel dimuat
+    setColumnWidths();
+
     $('#listproduct').DataTable({
         "ajax": {
             "url": "assets/data/product.json",
