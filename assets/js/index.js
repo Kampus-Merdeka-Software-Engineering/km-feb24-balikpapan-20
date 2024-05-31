c6ddbc08a1bdf88df927f2c0b95d4f1f40693aa0
fadhil9 <fadhilmuhamad272@gmail.com>
2024-05-31 16:29:58 +0700

    update navbar tablet

--- assets/js/index.js
+++ assets/js/index.js
@@ -212,3 +212,14 @@ signout.addEventListener('click',function(){
   localStorage.removeItem('loggedInUserId');
   signOut(auth);
 })
+
+// hamburger menu
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburgerMenu = document.getElementById('hamburger-menu');
+  const navLinks = document.getElementById('nav-links');
+
+  hamburgerMenu.addEventListener('click', () => {
+      navLinks.classList.toggle('active');
+  });
+});
+
