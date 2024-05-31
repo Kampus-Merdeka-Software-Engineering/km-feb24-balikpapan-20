1309b4ccf109b1adeb9a8d5e5d1fb22e6da4bf71
Yogi Kurnia Putra <168409372+tuanmudayogi@users.noreply.github.com>
2024-05-31 09:50:18 +0700

    Update product.js

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,46 +1,60 @@
 $(document).ready(function() {
-    // Fungsi untuk mengatur lebar kolom
-    function setColumnWidths() {
-        $('#listproduct thead th').each(function() {
-            var textWidth = $(this).textWidth();
-            $(this).width(textWidth + 60); // Tambahkan padding tambahan jika diperlukan
-        });
-    }
-
     // Fungsi untuk mengukur lebar teks
     $.fn.textWidth = function() {
         var htmlOrg = $(this).html();
-        var htmlCalc = '<span>' + htmlOrg + '</span>';
-        $(this).html(htmlCalc);
-        var width = $(this).find('span:first').width();
-        $(this).html(htmlOrg);
+        var htmlCalc = $('<span>' + htmlOrg + '</span>').css({
+            "font-size": $(this).css('font-size'),
+            "visibility": "hidden",
+            "white-space": "nowrap"
+        }).appendTo('body');
+        var width = htmlCalc.width();
+        htmlCalc.remove();
         return width;
     };
 
-    // Panggil fungsi setColumnWidths saat tabel dimuat
-    setColumnWidths();
-
+    // Inisialisasi DataTable
     $('#listproduct').DataTable({
         "ajax": {
             "url": "assets/data/product.json",
             "dataSrc": ""
         },
         "columns": [
-            {   
-                "data": "id_product",
-                "render": function (data, type, row) {
-                    return "ID-" + data; // Menambahkan "ID" di depan data id_product
-                }
-            },
-            {   "data": "product" },
-            {   "data": "category" },
-            { 
-                "data": "price",
-                "render": function (data, type, row) {
+            { "data": "no" },
+            { "data": "transaction_date" },
+            { "data": "transaction_time" },
+            { "data": "transaction_qty" },
+            { "data": "store_id" },
+            { "data": "store_location" },
+            { "data": "product_id" },
+            {
+                "data": "unit_price",
+                "render": function(data, type, row) {
                     return "$" + data;
                 }
             },
-            { "data": "type" }
-        ]
+            { "data": "product_category" },
+            { "data": "product_type" },
+            { "data": "product_detail" }
+        ],
+        "scrollY": "400px",
+        "scrollX": true,
+        "scrollCollapse": true,
+        "paging": true,
+        "autoWidth": false,
+        "fixedHeader": true,
+        "fixedColumns": true
+    });
+
+    // Fungsi untuk mengatur lebar kolom
+    function setColumnWidths() {
+        $('#listproduct thead th').each(function() {
+            var textWidth = $(this).textWidth();
+            $(this).width(textWidth + 100); // Tambahkan padding tambahan jika diperlukan
+        });
+    }
+
+    // Callback setelah DataTable selesai diinisialisasi
+    $('#listproduct').on('init.dt', function() {
+        setColumnWidths();
     });
 });
